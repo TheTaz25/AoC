c892cdc92b147185341c8030c01c47d5d9d38f51
Denis Ergin <denis.ergin@diva-e.com>
2022-11-23 13:32:06 +0100

    Y2021_D1_Part1

--- Y2021/D1/entry.py
+++ Y2021/D1/entry.py
@@ -1,1 +1,20 @@
 # https://adventofcode.com/2021/day/1
+import sys
+
+def prepareTask():
+  data = open('./Y2021/D1/input', 'r')
+  return [int(x) for x in data.read().splitlines()]
+
+def executeTask1(dataList):
+  increases = 0
+  previous = dataList[0]
+  for entry in dataList:
+    if previous < entry:
+      increases = increases + 1
+    previous = entry
+
+  print(increases)
+
+if __name__ == '__main__':
+  dataList = prepareTask()
+  executeTask1(dataList)
