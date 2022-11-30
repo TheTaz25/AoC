552d553d08ec2a21fcc5d40bc9091dc4dac1238b
Denis Ergin <denis.ergin@diva-e.com>
2022-11-30 12:06:52 +0100

    Current state of implementation Y2021/D3/P2

--- Y2021/D3/entry.py
+++ Y2021/D3/entry.py
@@ -16,7 +16,7 @@ def binaryConverter(binaryData):
 def flipBits(binaryData):
   return [1 if bit == 0 else 0 for bit in binaryData]
 
-def getDiagnosticDataSummary(diagnosticData: list[list[int]]):
+def getDiagnosticDataSummary(diagnosticData: list[list[str]]):
   mostCommonBitsInDiagnosticData = [0] * len(diagnosticData[0])
   for datum in diagnosticData:
     mostCommonBitsInDiagnosticData = [bitTrack + int(dataBit) for bitTrack, dataBit in zip(mostCommonBitsInDiagnosticData, datum)]
@@ -35,6 +35,40 @@ def calculateSubmarinePowerConsumption(diagnosticData: list[list[str]]):
   epsilonRate = binaryConverter(flipBits(condensedDiagnosticData))
   print("Power consumption is:", gammaRate * epsilonRate)
 
+def getOxygenGeneratorRating(diagnosticData: list[list[str]]):
+  candidates = diagnosticData
+  # while len(candidates) != 1:
+  #   pass
+  return candidates[0]
+
+def getCO2ScrubberRating(diagnosticData: list[list[str]]):
+  candidates = diagnosticData
+  currentPosition = 0
+  while len(candidates) != 1:
+    candidateSummary = getDiagnosticDataSummary(candidates)
+    print("candidateSummary", candidateSummary[currentPosition])
+    print("len of candidates", len(candidates))
+    x = flipBits([1 if positionBitCounter >= len(candidates) // 2 else 0 for positionBitCounter in candidateSummary])
+    print("x", x[currentPosition])
+    candidates = [candidate for candidate in candidates if int(candidate[currentPosition]) == x[currentPosition]]
+    currentPosition = currentPosition + 1
+  print('candidate', candidates[0])
+  return candidates[0]
+
+def getLifeSupportReports(diagnosticData: list[list[str]]):
+  oxygenGeneratorRating = getOxygenGeneratorRating(diagnosticData)
+  cO2ScrubberRating = getCO2ScrubberRating(diagnosticData)
+
+  print(oxygenGeneratorRating, cO2ScrubberRating)
+
+  return [oxygenGeneratorRating, cO2ScrubberRating]
+
+def calculateLifeSupportRating(diagnosticData: list[list[str]]):
+  [oxygenGeneratorRating, cO2ScrubberRating] = getLifeSupportReports(diagnosticData)
+
+  print("Current lifesupport-rating is:", oxygenGeneratorRating * cO2ScrubberRating)
+
 if __name__ == '__main__':
   diagnosticReport = prepareTask()
   calculateSubmarinePowerConsumption(diagnosticReport)
+  calculateLifeSupportRating(diagnosticReport)
