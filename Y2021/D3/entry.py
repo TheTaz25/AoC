86cca86507ffdf062a8312db9627009fbc9a8caa
Denis Ergin <denis.ergin@diva-e.com>
2022-12-02 09:21:35 +0100

    Start rework Y2021/D3/*

--- Y2021/D3/entry.py
+++ Y2021/D3/entry.py
@@ -2,73 +2,30 @@
 
 def prepareTask():
   raw = open('./Y2021/D3/input', 'r')
-  return [list(line) for line in raw.read().splitlines()]
+  return [line for line in raw.read().splitlines()]
 
-def binaryConverter(binaryData):
-  exponent = 0
-  result = 0
+def getBitCountOnPosition(dataList: list[str], position: str):
+  ones = 0
+  zeroes = 0
+  for entry in dataList:
+    if int(entry, 2) & int(position, 2):
+      ones = ones + 1
+    else:
+      zeroes = zeroes + 1
+  return [ones, zeroes]
 
-  for bit in binaryData:
-    result = result + ((2 ** exponent) * bit)
-    exponent = exponent + 1
-  return result
-  
-def flipBits(binaryData):
-  return [1 if bit == 0 else 0 for bit in binaryData]
-
-def getDiagnosticDataSummary(diagnosticData: list[list[str]]):
-  mostCommonBitsInDiagnosticData = [0] * len(diagnosticData[0])
-  for datum in diagnosticData:
-    mostCommonBitsInDiagnosticData = [bitTrack + int(dataBit) for bitTrack, dataBit in zip(mostCommonBitsInDiagnosticData, datum)]
-  return mostCommonBitsInDiagnosticData
-
-def calculateSubmarinePowerConsumption(diagnosticData: list[list[str]]):
-  halfLengthOfDiagnosticData = len(diagnosticData) // 2
-
-  mostCommonBitsInDiagnosticData = getDiagnosticDataSummary(diagnosticData)
-
-  condensedDiagnosticData = [1 if positionBitCounter > halfLengthOfDiagnosticData else 0 for positionBitCounter in mostCommonBitsInDiagnosticData]
-  # We want to start with the least significant bit when converting from binary data to decimal data
-  condensedDiagnosticData.reverse()
-
-  gammaRate = binaryConverter(condensedDiagnosticData)
-  epsilonRate = binaryConverter(flipBits(condensedDiagnosticData))
-  print("Power consumption is:", gammaRate * epsilonRate)
-
-def getOxygenGeneratorRating(diagnosticData: list[list[str]]):
-  candidates = diagnosticData
-  # while len(candidates) != 1:
-  #   pass
-  return candidates[0]
-
-def getCO2ScrubberRating(diagnosticData: list[list[str]]):
-  candidates = diagnosticData
-  currentPosition = 0
-  while len(candidates) != 1:
-    candidateSummary = getDiagnosticDataSummary(candidates)
-    print("candidateSummary", candidateSummary[currentPosition])
-    print("len of candidates", len(candidates))
-    x = flipBits([1 if positionBitCounter >= len(candidates) // 2 else 0 for positionBitCounter in candidateSummary])
-    print("x", x[currentPosition])
-    candidates = [candidate for candidate in candidates if int(candidate[currentPosition]) == x[currentPosition]]
-    currentPosition = currentPosition + 1
-  print('candidate', candidates[0])
-  return candidates[0]
-
-def getLifeSupportReports(diagnosticData: list[list[str]]):
-  oxygenGeneratorRating = getOxygenGeneratorRating(diagnosticData)
-  cO2ScrubberRating = getCO2ScrubberRating(diagnosticData)
-
-  print(oxygenGeneratorRating, cO2ScrubberRating)
-
-  return [oxygenGeneratorRating, cO2ScrubberRating]
-
-def calculateLifeSupportRating(diagnosticData: list[list[str]]):
-  [oxygenGeneratorRating, cO2ScrubberRating] = getLifeSupportReports(diagnosticData)
-
-  print("Current lifesupport-rating is:", oxygenGeneratorRating * cO2ScrubberRating)
+def getPowerConsumption(report: list[str]):
+  position = bin(1 << len(report[0]) - 1)
+  binaryReport = [bin(int(datum, 2)) for datum in report]
+  while position:
+    position >> 1
+  print(position)
+  print(binaryReport)
+  print(getBitCountOnPosition(binaryReport, position))
+  pass
 
 if __name__ == '__main__':
   diagnosticReport = prepareTask()
-  calculateSubmarinePowerConsumption(diagnosticReport)
-  calculateLifeSupportRating(diagnosticReport)
+  print(diagnosticReport)
+
+  getPowerConsumption(diagnosticReport)
