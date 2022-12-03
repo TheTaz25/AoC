feff86e6eb26073023323768645c97e3bf8f12d9
Denis Ergin <denis.ergin@diva-e.com>
2022-12-03 11:13:14 +0100

    Y2022/D3/P2

--- Y2022/D3/entry.py
+++ Y2022/D3/entry.py
@@ -3,7 +3,7 @@ import re
 
 def prepareTask():
   data = open('./Y2022/D3/input', 'r')
-  return [[datum[0:len(datum)//2], datum[len(datum)//2::]] for datum in data.read().splitlines()]
+  return [datum for datum in data.read().splitlines()]
 
 def convertPriorities(rawPriorities: list[str]):
   a = ord('a') - 1
@@ -13,7 +13,19 @@ def getPrioritiesFromRucksacks(rucksacks: list[list[str]]):
   rawPriorities = [re.compile(r'['+re.escape(compartment1)+r']').search(compartment2).group() for [compartment1, compartment2] in rucksacks]
   return convertPriorities(rawPriorities)
 
+def getCommonLetterInTriplet(triplet: list[str]):
+  one, two, three = [set(list(section)) for section in triplet]
+  return list(one.intersection(two).intersection(three))[0]
+
+def getElfGroupPriorities(rucksacks: list[str]):
+  triplets = [rucksacks[offset:offset+3] for offset in range(0, len(rucksacks), 3)]
+  return convertPriorities([getCommonLetterInTriplet(triplet) for triplet in triplets])
+
 if __name__ == '__main__':
   rucksacks = prepareTask()
-  priorities = getPrioritiesFromRucksacks(rucksacks)
-  print(sum(priorities))
+
+  priorities = getPrioritiesFromRucksacks([[sack[0:len(sack)//2], sack[len(sack)//2::]] for sack in rucksacks])
+  print("The sum of all priorities is:", sum(priorities))
+
+  elfGroupPriorities = getElfGroupPriorities(rucksacks)
+  print("The sum of all group priorities is:", sum(elfGroupPriorities))
